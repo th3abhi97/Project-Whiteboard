c4eaf249f3dac6f5710f15f0cfb6d06277fbd8c5
n00bs <abhi.cool10@yahoo.com>
2017-02-16 20:16:43 -0800

    indentation changes

--- manager/auth.php
+++ manager/auth.php
@@ -1,32 +1,32 @@
 <?php
-  require_once 'php-jwt/src/BeforeValidException.php';
-  require_once 'php-jwt/src/ExpiredException.php';
-  require_once 'php-jwt/src/SignatureInvalidException.php';
-  require_once 'php-jwt/src/JWT.php';
-  include 'config.php';
+    require_once 'php-jwt/src/BeforeValidException.php';
+    require_once 'php-jwt/src/ExpiredException.php';
+    require_once 'php-jwt/src/SignatureInvalidException.php';
+    require_once 'php-jwt/src/JWT.php';
+    include 'config.php';
 
-  use \Firebase\JWT\JWT;
+    use \Firebase\JWT\JWT;
 
-  // Check if the user is logged in
-  $uid = $_SERVER['REMOTE_USER'];
-  if(isset($uid)) {
-    create_custom_token($uid);
-  } else {
-    die("AUTH FAILURE");
-  }
+    // Check if the user is logged in
+    $uid = $_SERVER['REMOTE_USER'];
+    if(isset($uid)) {
+        create_custom_token($uid);
+    } else {
+        die("AUTH FAILURE");
+    }
 
-  function create_custom_token($uid) {
-    global $service_account_email, $private_key;
+    function create_custom_token($uid) {
+        global $service_account_email, $private_key;
 
-    $now_seconds = time();
-    $payload = array(
-      "iss" => $service_account_email,
-      "sub" => $service_account_email,
-      "aud" => "https://identitytoolkit.googleapis.com/google.identity.identitytoolkit.v1.IdentityToolkit",
-      "iat" => $now_seconds,
-      "exp" => $now_seconds+(60*60),  // Maximum expiration time is one hour
-      "uid" => $uid
-    );
-    echo JWT::encode($payload, $private_key, "RS256");
-  }
+        $now_seconds = time();
+        $payload = array(
+            "iss" => $service_account_email,
+            "sub" => $service_account_email,
+            "aud" => "https://identitytoolkit.googleapis.com/google.identity.identitytoolkit.v1.IdentityToolkit",
+            "iat" => $now_seconds,
+            "exp" => $now_seconds+(60*60),  // Maximum expiration time is one hour
+            "uid" => $uid
+        );
+        echo JWT::encode($payload, $private_key, "RS256");
+    }
 ?>
--- manager/main.js
+++ manager/main.js
@@ -1,14 +1,14 @@
 var xmlHttp = new XMLHttpRequest();
 xmlHttp.onload = function() {
-  var token = xmlHttp.responseText;
-  console.log("token : " + token);
-  // Authenticate
-  firebase.auth().signInWithCustomToken(token).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorCode +  " : " + errorMessage);
-  });
+    var token = xmlHttp.responseText;
+    console.log("token : " + token);
+    // Authenticate
+    firebase.auth().signInWithCustomToken(token).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode +  " : " + errorMessage);
+    });
 }
 // false for synchronous request
 xmlHttp.open("GET", "auth.php", true);
