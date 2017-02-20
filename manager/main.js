a44fabf288b5a8091e79f895f778a4fd57fe704c
n00bs <abhi.cool10@yahoo.com>
2017-02-20 03:44:04 -0800

    getting started with basic layout of the ui

--- manager/main.js
+++ manager/main.js
@@ -1,15 +1,37 @@
-var xmlHttp = new XMLHttpRequest();
-xmlHttp.onload = function() {
-    var token = xmlHttp.responseText;
-    console.log("token : " + token);
-    // Authenticate
-    firebase.auth().signInWithCustomToken(token).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode +  " : " + errorMessage);
-    });
+"use strict";
+
+// perform all necessary steps here
+window.onload = function() {
+    var my_events = document.getElementById("my-events");
+    var all_events = document.getElementById("all-events");
+    /*my_events.onclick = function() {
+        console.log(this);
+        var loading_modal = document.getElementById("loading-modal");
+        loading_modal.classList.remove("is-active");
+    };*/
 }
-// false for synchronous request
-xmlHttp.open("GET", "auth.php", true);
-xmlHttp.send(null);
+
+// authenticate the user
+function authenticate() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onload = function() {
+        if (xmlHttp.status == 200) {
+            var token = xmlHttp.responseText;
+            // Authenticate
+            firebase.auth().signInWithCustomToken(token).catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorCode +  " : " + errorMessage);
+            });
+        }
+    }
+    // False for synchronous request
+    xmlHttp.open("GET", "auth.php", false);
+    xmlHttp.send(null);
+}
+
+// Get data and populate the window
+function getData() {
+
+}
